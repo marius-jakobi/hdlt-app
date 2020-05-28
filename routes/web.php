1bf46c851ff288fdcf7a37934de888cde795cb44
Marius Jakobi <marius.jakobi@googlemail.com>
2020-05-28 10:55:34 +0200

    Extracted Auth::routes() for better access control

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
+// Login
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+// Registration
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\RegisterController@register');
+// Logout
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+// Password confirmation
+Route::get('/password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('/password/confirm', 'Auth\ConfirmPasswordController@confirm');
+// Forgot password
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+// Dashboard
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
