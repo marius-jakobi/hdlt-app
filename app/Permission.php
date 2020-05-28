4e40ed910188b7ccedb93a376f47ff472644e88f
Marius Jakobi <marius.jakobi@googlemail.com>
2020-05-28 13:05:36 +0200

    Created models

---
+++ app/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    // This model needs no timestamps
+    public $timestamps = false;
+}
---
+++ app/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    // This model needs no timestamps
+    public $timestamps = false;
+}
