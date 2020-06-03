0d273907581c4506818d09f37216e7249390b3bd
Marius Jakobi <marius.jakobi@googlemail.com>
2020-06-03 10:06:45 +0200

    Cleaned up seeder

--- database/seeds/RolesPermissionsSeeder.php
+++ database/seeds/RolesPermissionsSeeder.php
@@ -14,12 +14,7 @@ class RolesPermissionsSeeder extends Seeder
     public function run()
     {
         $admins = Role::create(['name' => 'administrator', 'description' => 'Administratoren']);
-        $technicians = Role::create(['name' => 'technician', 'description' => 'Service-Techniker']);
-        $backoffice = Role::create(['name' => 'back-office', 'description' => 'Mitarbeiter im Innendienst']);
-        $sales = Role::create(['name' => 'sales-agents', 'description' => 'Mitarbeiter im Außendienst']);
 
         $createUsers = Permission::create(['name' => 'create-users', 'description' => 'Die Rolle kann neue Benutzer erstellen']);
-        $collectStationData = Permission::create(['name' => 'collect-station-data', 'description' => 'Die Rolle kann Anlagen aufnehmen']);
-        $executeJobs = Permission::create(['name' => 'execute-jobs', 'description' => 'Die Rolle kann Aufträge durchführen']);
     }
 }
