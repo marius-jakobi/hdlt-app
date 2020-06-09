1f349d7f2bc8f62184ae1029d204a3893c2d4e92
Marius Jakobi <marius.jakobi@googlemail.com>
2020-06-09 08:54:36 +0200

    Show all station components

--- resources/views/customer/addresses/shipping/details.blade.php
+++ resources/views/customer/addresses/shipping/details.blade.php
@@ -49,14 +49,13 @@
 
 <h2>Anlagen</h2>
 <h3>Kompressoren</h3>
+@if ($shippingAddress->compressors->count() > 0)
 <table class="table">
     <thead>
         <tr>
             <th>Hersteller</th>
             <th>Modell</th>
             <th>S/N</th>
-            <th>Druck</th>
-            <th>Leistung</th>
             <th>Baujahr</th>
             <th>Typ</th>
             <th>nächste Wartung</th>
@@ -68,8 +67,6 @@
             <td>{{ $compressor->brand->name }}</td>
             <td>{{ $compressor->model }}</td>
             <td>{{ $compressor->serial }}</td>
-            <td>{{ $compressor->pressure }}</td>
-            <td>{{ $compressor->power }}</td>
             <td>{{ $compressor->year }}</td>
             <td>{{ $compressor->getType() }}</td>
             <td>{{ $compressor->next_service }}</td>
@@ -77,7 +74,11 @@
         @endforeach
     </tbody>
 </table>
+@else
+<div class="alert alert-info">Es sind keine Kompressoren angelegt.</div>
+@endif
 <h3>Behälter</h3>
+@if ($shippingAddress->receivers->count() > 0)
 <table class="table">
     <thead>
         <tr>
@@ -103,12 +104,16 @@
         </tr>
         @endforeach
     </tbody>
-</table>
+</table>@else
+<div class="alert alert-info">Es sind keine Behälter angelegt.</div>
+@endif
 <h3>Kältetrockner</h3>
+@if ($shippingAddress->ref_dryers->count() > 0)
 <table class="table">
     <thead>
         <tr>
             <th>Hersteller</th>
+            <th>Modell</th>
             <th>S/N</th>
             <th>Baujahr</th>
             <th>Kältemittel</th>
@@ -119,6 +124,7 @@
         @foreach ($shippingAddress->ref_dryers as $ref_dryer)
         <tr>
             <td>{{ $ref_dryer->brand->name }}</td>
+            <td>{{ $ref_dryer->model }}</td>
             <td>{{ $ref_dryer->serial }}</td>
             <td>{{ $ref_dryer->year }}</td>
             <td>{{ $ref_dryer->getRefrigerant() }}</td>
@@ -127,4 +133,159 @@
         @endforeach
     </tbody>
 </table>
+@else
+<div class="alert alert-info">Es sind keine Kältetrockner angelegt.</div>
+@endif
+<h3>Filter</h3>
+@if ($shippingAddress->filters->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Filtertasse</th>
+            <th>Filterelement</th>
+            <th>nächste Prüfung</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->filters as $filter)
+        <tr>
+            <td>{{ $filter->brand->name }}</td>
+            <td>{{ $filter->model }}</td>
+            <td>{{ $filter->element }}</td>
+            <td>{{ $filter->next_service }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine Filter angelegt.</div>
+@endif
+<h3>Adsorptionstrockner</h3>
+@if ($shippingAddress->ad_dryers->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Modell</th>
+            <th>S/N</th>
+            <th>Baujahr</th>
+            <th>nächste Prüfung</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->ad_dryers as $ad_dryer)
+        <tr>
+            <td>{{ $ad_dryer->brand->name }}</td>
+            <td>{{ $ad_dryer->model }}</td>
+            <td>{{ $ad_dryer->serial }}</td>
+            <td>{{ $ad_dryer->year }}</td>
+            <td>{{ $ad_dryer->next_service }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine Adsorptionstrockner angelegt.</div>
+@endif
+<h3>Öldampfadsorber</h3>
+@if ($shippingAddress->adsorbers->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Modell</th>
+            <th>S/N</th>
+            <th>Baujahr</th>
+            <th>nächste Prüfung</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->adsorbers as $adsorber)
+        <tr>
+            <td>{{ $adsorber->brand->name }}</td>
+            <td>{{ $adsorber->model }}</td>
+            <td>{{ $adsorber->serial }}</td>
+            <td>{{ $adsorber->year }}</td>
+            <td>{{ $adsorber->next_service }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine Öldampfadsorber angelegt.</div>
+@endif
+<h3>Öl-Wasser-Trenner</h3>
+@if ($shippingAddress->separators->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Modell</th>
+            <th>nächste Prüfung</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->separators as $separator)
+        <tr>
+            <td>{{ $separator->brand->name }}</td>
+            <td>{{ $separator->model }}</td>
+            <td>{{ $separator->next_service }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine Öl-Wasser-Trenner angelegt.</div>
+@endif
+<h3>Stationssensoren</h3>
+@if ($shippingAddress->sensors->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Modell</th>
+            <th>S/N</th>
+            <th>Baujahr</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->sensors as $sensor)
+        <tr>
+            <td>{{ $sensor->brand->name }}</td>
+            <td>{{ $sensor->model }}</td>
+            <td>{{ $sensor->serial }}</td>
+            <td>{{ $sensor->year }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine Stationssensoren angelegt.</div>
+@endif
+<h3>Übergeordnete Steuerungen</h3>
+@if ($shippingAddress->controllers->count() > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th>Hersteller</th>
+            <th>Modell</th>
+            <th>S/N</th>
+            <th>Baujahr</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($shippingAddress->controllers as $controller)
+        <tr>
+            <td>{{ $controller->brand->name }}</td>
+            <td>{{ $controller->model }}</td>
+            <td>{{ $controller->serial }}</td>
+            <td>{{ $controller->year }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+<div class="alert alert-info">Es sind keine übergeordneten Steuerungen angelegt.</div>
+@endif
 @endsection
