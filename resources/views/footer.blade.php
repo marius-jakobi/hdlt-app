0c2c5b578a0a702034a578177199856a643b6b3f
Marius Jakobi <marius.jakobi@googlemail.com>
2020-06-24 16:46:45 +0200

    Styling

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,17 @@
 <footer class="footer">
-    <div class="bg-secondary p-5">
+    <div class="bg-secondary p-3">
         <div class="container">
-            &copy; {{ date('Y') }} Herbst Drucklufttechnik GmbH
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <p>
+                        &copy; {{ date('Y') }} Herbst Drucklufttechnik GmbH
+                    </p>
+                </div>
+                <div class="col-sm-12 col-md-6 text-md-right">
+                    <p>Impressum</p>
+                    <p>Datenschutz</p>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
