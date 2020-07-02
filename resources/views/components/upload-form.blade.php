ba1f557b41910aeabc55b129e309135d250b5e80
Marius Jakobi <marius.jakobi@googlemail.com>
2020-07-02 15:21:36 +0200

    Added 'required'

--- resources/views/components/upload-form.blade.php
+++ resources/views/components/upload-form.blade.php
@@ -2,14 +2,14 @@
     @csrf
     <div class="form-group">
         <label>Beschreibung</label>
-        <input type="text" name="name" class="form-control @error('name', 'files') is-invalid @enderror" minlength="3" maxlength="255">
+        <input type="text" name="name" class="form-control @error('name', 'files') is-invalid @enderror" minlength="4" maxlength="255" required>
         <small class="form-text text-muted">Diese Beschreibung wird allen Bildern zugeordnet, die in einem Vorgang hochgeladen werden.</small>
     </div>
     @error('name', 'files')
         <p class="text-danger">{{ $message }}
     @enderror
     <div class="form-group">
-        <input type="file" name="files[]" class="w-100 btn btn-secondary @error('files', 'files') text-danger @enderror " multiple>
+        <input type="file" name="files[]" class="w-100 btn btn-secondary @error('files', 'files') text-danger @enderror " multiple required>
     </div>
     @error('files', 'files')
         <p class="text-danger">{{ $message }}</p>
