a107990622660754590f39de0c901fb7916bd9c9
Marius Jakobi <marius.jakobi@googlemail.com>
2020-07-07 15:20:02 +0200

    Show intent and text

--- resources/views/processes/sales/service-report/details.blade.php
+++ resources/views/processes/sales/service-report/details.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     <h1>Service-Bericht</h1>
     <p>ID: {{ $report->id }}</p>
+    <p>Einsatzzweck: {{ $report->intent }}</p>
+    <p>Text: {{ $report->text }}</p>
 @endsection
